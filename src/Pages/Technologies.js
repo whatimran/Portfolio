6dd9849240fcffdc7872a38df1c1819174e88e9c
Imran Zia <77832096+whatimran@users.noreply.github.com>
2024-05-07 13:38:57 +0400

    Update Technologies.js

--- src/Pages/Technologies.js
+++ src/Pages/Technologies.js
@@ -5,10 +5,6 @@ function Technologies() {
   const {
     html,
     css,
-    js,
-    react,
-    redux,
-    tailwind,
     bootstrap,
     sass,
     vscode,
@@ -22,7 +18,7 @@ function Technologies() {
     <main className="container mx-auto max-width pt-10 pb-20 ">
       <section>
         <h1 className="text-2xl text-dark-heading dark:text-light-heading md:text-4xl xl:text-5xl xl:leading-tight font-bold">
-          Tech Stack
+          Technologies
         </h1>
         <p className="text-content py-2 lg:max-w-3xl">
           Technologies I've been working with recently
@@ -31,10 +27,8 @@ function Technologies() {
       <section className="grid grid-cols-4 md:grid-cols-5 lg:grid-cols-6 items-center gap-10 pt-6">
         <img src={html} title="html" alt="" />
         <img src={css} title="CSS" alt="" />
-        <img src={js} title="JavaScript" alt="" />
-        <img src={react} title="React" alt="" />
-        <img src={redux} title="Redux" alt="" />
-        <img src={tailwind} title="Tailwind CSS" alt="" />
+        <img src={php} title="PHP" alt="" />
+        <img src={python} title="Python" alt="" />
         <img src={bootstrap} title="Bootstrap" alt="" />
         <img src={sass} title="SASS" alt="" />
       </section>
