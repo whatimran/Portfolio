55e1fafec0c63e718a614f0055a0339b61c023ed
Imran Zia <77832096+whatimran@users.noreply.github.com>
2024-05-07 12:17:47 +0400

    Update Project.js

--- src/Components/Project.js
+++ src/Components/Project.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function Project({ title, image, description, techstack, previewLink, githubLink }) {
+function Project({ title, image, description, is, previewLink, githubLink }) {
   return (
     <article className="rounded-xl mt-10 overflow-hidden shadow-xl shadow-slate-300 dark:shadow-slate-900">
       <img src={image} alt="" loading="lazy" />
@@ -8,7 +8,7 @@ function Project({ title, image, description, techstack, previewLink, githubLink
         <h1 className="dark:text-light-heading font-semibold text-lg pt-1">{title}</h1>
         <p className="text-content pt-4 font-light">{description}</p>
         <h3 className="text-dark-heading dark:text-light-heading font-medium pt-4">
-          Tech Stack : <span className="font-light">{techstack}</span>
+          IS : <span className="font-light">{is}</span>
         </h3>
         <div className="flex justify-between items-center mt-5">
           <div className="flex items-center">
