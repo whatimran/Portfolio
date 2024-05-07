c7797933699e13ebb8882fbd11394c24744327b8
Imran Zia <77832096+whatimran@users.noreply.github.com>
2024-05-07 10:57:31 +0400

    Add files via upload

---
+++ src/Details.js
@@ -0,0 +1,145 @@
+// Logo images
+import logogradient from "./assets/logo.svg";
+import logo from "./assets/logo2.svg";
+// Profile Image
+import profile from "./assets/profile.jpg";
+// Images
+import html from "./assets/is/html.png";
+import css from "./assets/is/css.png";
+import php from "./assets/is/php.png";
+import python from "./assets/is/python.png";
+import sass from "./assets/is/sass.png";
+import js from "./assets/is/js.png";
+import react from "./assets/is/react.png";
+import redux from "./assets/is/redux.png";
+import tailwind from "./assets/is/tailwind.png";
+import bootstrap from "./assets/is/bootstrap.png";
+import vscode from "./assets/is/vscode.png";
+import github from "./assets/is/github.png";
+import git from "./assets/is/git.png";
+import npm from "./assets/is/npm.png";
+import postman from "./assets/is/postman.png";
+import figma from "./assets/is/figma.png";
+// Project Images
+import projectImage1 from "./assets/projects/project1.png";
+import projectImage2 from "./assets/projects/project2.png";
+import projectImage3 from "./assets/projects/project3.png";
+import projectImage4 from "./assets/projects/project4.png";
+
+// Logos
+export const logos = {
+  logogradient: logogradient,
+  logo: logo,
+};
+
+// Personal Details
+export const personalDetails = {
+  name: "Syed Imran Hussain Zia",
+  tagline: "I build things for web",
+  img: profile,
+  about: "Transforming ideas into digital success. Web developer, e-commerce whiz, digital marketer, freelancer.",
+};
+
+// Social Media URLs
+export const socialMediaUrl = {
+  linkdein: "https://www.linkedin.com/in/syed-imran-hussain-zia/",
+  github: "https://www.github.com/whatimran",
+  twitter: "https://twitter.com/whoimran",
+  instagram: "https://www.instagram.com/dxb.imrann",
+};
+
+// Work Experience
+export const workDetails = [
+  {
+    Position: "Digital Marketing Specialist",
+    Company: "Gamers Tycoon Technology LLC",
+    Location: "Dubai, UAE.",
+    Type: "Full Time",
+    Duration: "Sep 2023 - Present",
+  },
+  {
+    Position: "Administrative Assistant",
+    Company: "Rawalpindi Polytechnic Institute",
+    Location: "Rawalpindi, Pakistan.",
+    Type: "Full Time",
+    Duration: "Feb 2023 - Jul 2023",
+  },
+  {
+    Position: "Data Entry Operator",
+    Company: "ONELINK Service Provider",
+    Location: "Islamabad, Pakistan.",
+    Type: "Full Time",
+    Duration: "Oct 2020 - Nov 2022",
+  },
+];
+
+// Education Details
+export const eduDetails = [
+  {
+    Position: "Bachelor of Science in Information Technology",
+    Company: "Virtual University",
+    Location: "Rawalpindi, Pakistan.",
+    Type: "Full Time",
+    Duration: "2020 - 2024",
+  },
+    {
+    Position: "Diploma in Information Technology",
+    Company: "National Training Bureau",
+    Location: "Islamabad, Pakistan.",
+    Type: "Full Time",
+    Duration: "2018 - 2019",
+  },
+];
+
+// Tech Stack and Tools
+export const isDetails = {
+  html: html,
+  css: css,
+  php: php,
+  python: python,
+  bootstrap: bootstrap,
+  vscode: vscode,
+  postman: postman,
+  npm: npm,
+  git: git,
+  github: github,
+  figma: figma,
+};
+
+// Project Details
+export const projectDetails = [
+  {
+    title: "DXB Gamers",
+    image: projectImage1,
+    description: "DXB Gamers is a leading computer parts provider in UAE which deals in multiple categories.",
+    is: "PHP/CSS, Wordpress",
+    previewLink: "https://dxbgamers.com",
+  },
+  {
+    title: "Next Level Car Rental",
+    image: projectImage2,
+    description: "Next Level Car Rental is a UAE based trusted and reliable car rental solution.",
+    is: "PHP/CSS, Wordpress",
+    previewLink: "https://nextlevelcar.ae",
+  },
+  {
+    title: "Altaysir Travel Agency",
+    image: projectImage3,
+    description: "Al Taysir Travel and Tourism Agency is a Dubai based company with 16 years of customer satisfaction.",
+    is: "PHP/CSS, Wordpress",
+    previewLink: "https://altaysir.net",
+  },
+  {
+    title: "GoogleIndexer",
+    image: projectImage4,
+    description: "Index URLs to Google Search Engine with Google API Indexer.",
+    is: "Python",
+	githubLink: "https://github.com/whatimran/GoogleIndexer",
+  },
+];
+
+// Contact Details
+export const contactDetails = {
+  email: "imirwp@gmail.com",
+  phone: "+971 52 3132998",
+};
