c09b39049aa556b695e09eb5d99e857de58492d9
Imran Zia <77832096+whatimran@users.noreply.github.com>
2024-05-07 12:16:51 +0400

    Update Footer.js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -6,11 +6,11 @@ function Footer() {
         Designed and Coded by{" "}
         <a
           className="font-medium"
-          href="https://pavanmg.in"
+          href="https://imranzia.com"
           target="_blank"
           rel="noreferrer noopener"
         >
-          Pavan MG
+          Imran Shah
         </a>{" "}
         with
         <span className="text-gradient font-medium"> Love</span> &
