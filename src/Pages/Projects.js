249fab68394d888ea0ce58dfacee46f7cdb01d21
Imran Zia <77832096+whatimran@users.noreply.github.com>
2025-01-13 19:35:47 +0400

    Update Projects.js

--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -12,7 +12,7 @@ function Projects() {
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-10">
           {React.Children.toArray(
             projectDetails.map(
-              ({ title, image, description, is, previewLink }) => (
+              ({ title, image, description, tech, previewLink }) => (
                 <Project
                   title={title}
                   image={image}
